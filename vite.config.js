06f9ca1cb0a236777a344d761cb52d014334298b
Sean Washington <hello@seanwash.com>
2023-02-05 09:24:51 -0800

    Update vite

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+
+const host = "seanwash.com.test";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            valetTls: host,
             refresh: true,
         }),
     ],
