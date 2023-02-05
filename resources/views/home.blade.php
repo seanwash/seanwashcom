c6d684c27306ef488474492ec570e175b7e0aaf0
Sean Washington <hello@seanwash.com>
2023-02-05 11:57:32 -0800

    Change favicon for no production environments

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,19 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link
-            rel="icon"
-            href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>🤖</text></svg>"
-        >
+        @production
+            <link
+                rel="icon"
+                href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>🤖</text></svg>"
+            >
+        @endproduction
+
+        @env('local', 'develop', 'staging')
+            <link
+                rel="icon"
+                href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>🚧</text></svg>"
+            >
+        @endenv
 
         <title>{{ config('app.name') }}</title>
 
