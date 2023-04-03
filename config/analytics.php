6f13fda60925cd62f5039d6fc2e95a904269443c
Sean Washington <hello@seanwash.com>
2023-04-02 20:13:44 -0700

    Remove Fathom analytics tracking

--- config/analytics.php
+++ config/analytics.php
@@ -2,10 +2,7 @@
 
 return [
     'enabled' => env('ANALYTICS_ENABLED', false),
-    'fathom' => [
-        'site_id' => env('FATHOM_SITE_ID'),
-        'script_url' => env('FATHOM_SCRIPT_URL', 'https://cdn.usefathom.com/script.js'),
-    ],
+
     'seanwash' => [
         'site_id' => env('ANALYTICS_SITE_ID'),
         'script_url' => env('ANALYTICS_SCRIPT_URL', 'https://analytics.seanwash.com/tracker.js'),
--- resources/views/components/analytics/fathom.blade.php
+++
@@ -1,6 +0,0 @@
-@if(config('analytics.enabled'))
-    <script
-        src="{{ config('analytics.fathom.script_url') }}"
-        data-site="{{ config('analytics.fathom.site_id') }}" defer
-    ></script>
-@endif
