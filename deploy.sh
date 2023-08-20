4738d7976f0d01a5a967a12269c0bb06e8931386
Sean Washington <hello@seanwash.com>
2023-08-19 22:06:17 -0700

    Setup CD

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ composer install --prefer-dist --optimize-autoloader --no-dev
 # Perform extra tasks for your framework of choice
 # (e.g. generate the framework cache)
 # [...]
+npm install
 npm run build
 
 # We do not want to deploy "dev" caches that were generated on our machine (because paths will be different on AWS Lambda).
